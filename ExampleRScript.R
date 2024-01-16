77df94019115b4c179d99b1a9486f812e992589b
KevinGSmith <142537366+KevinGSmith@users.noreply.github.com>
2024-01-16 08:42:09 -0500

    Add files via upload

---
+++ ExampleRScript.R
@@ -0,0 +1,12 @@
+#Hi! This document is an R "script". A script is sort of like a Word file, but
+#for code. You can write in this script and use it to edit your code and also to
+#take notes on your code, as well.
+
+#Because scripts usually include code, we have to designate anything that is NOT
+#code, so that RStudio won't try to run regular text as code. In R, the way we
+#do that is by including "#" at the beginning of a line. The "#" indicates that
+#we're just taking notes and not writing code.
+
+#Scripts are one type of R file that we'll be working with this semester. I'll
+#be asking you to complete your first pre-class work assignment, for Session 2,
+#in a script like this one.
